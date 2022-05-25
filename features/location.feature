fb9a1ab3f9d9abdb920a828aca3b2f412cff23bc
Olga Makaranka <olga.makaranka@testdevlab.com>
2022-05-25 23:04:13 +0300

    Final_task

---
+++ features/location.feature
@@ -0,0 +1,8 @@
+Feature: Location
+
+  Scenario: Create new location
+    Given user is authorized
+    Then user creates new location
+    And user updates location
+    Then user delete location
+    And user list all locations
---
+++ features/step_definitions/Locations_steps.rb
@@ -0,0 +1,21 @@
+Given(/^prepare_auth_token$/) do
+  prepare_auth_token
+end
+
+Then(/^user creates new location$/) do
+  create_location
+  retrieve_location
+end
+
+And(/^user updates location$/) do
+  update_location
+end
+
+Then(/^user delete location/) do
+  delete_location
+  retrieve_deleted_location
+end
+
+Then(/^user list all locations$/) do
+  list_all_locations
+end
